43d5ded7af7f32e518c855c4cb522feea07811b2
TP - 06LK - - <815066@pdsb.net>
2020-07-31 15:55:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+//Functions redirect to links when confirmation is confirmed
+function play() {
+  if (
+    confirm(
+      "This is going to take you to a new page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://play2048.co/");
+  }
+}
+function pac() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://worldsbiggestpacman.com/");
+  }
+}
+function slither() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("http://slither.io/");
+  }
+}
+function agar() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://agar.io/");
+  }
+}
+function invaders() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://elgoog.im/space-invaders/");
+  }
+}
+function dinoGame() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://elgoog.im/t-rex/?bot");
+  }
+}
+function pokemon() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://play.pokemonshowdown.com/");
+  }
+}
+function minecraft() {
+  if (
+    confirm(
+      "This is going to take you to a different page. Are you sure you want to do this?"
+    )
+  ) {
+    window.open("https://classic.minecraft.net/");
+  }
+}
+
